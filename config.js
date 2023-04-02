954399b1bf8abb1795af6d4e82ad170c77837b5f
Sasha Kharkova <alexandra.filipceva@yandex.ru>
2023-04-02 21:15:06 +0300

    Empty commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const DB_ADDRESS = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/mestodb';
 
 if (process.env.NODE_ENV === "production") {
